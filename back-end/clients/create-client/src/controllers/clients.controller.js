881646288f7499c0dd0ec661c0fdcf1c4454feae
AlexMorales <admoralesp@uce.edu.ec>
2025-06-14 22:07:21 -0500

    feat: :sparkles: Clean up ignored files and update .gitignore and config create-pet

--- back-end/clients/create-client/src/controllers/clients.controller.js
+++ back-end/clients/create-client/src/controllers/clients.controller.js
@@ -25,3 +25,20 @@ exports.createClient = async (req, res, next) => {
     next(err); // 👈 Esto manda el error al manejador global
   }
 };
+
+exports.getClientById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query('SELECT * FROM clients WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'Client not found' });
+    }
+
+    res.status(200).json({ client: result.rows[0] });
+  } catch (err) {
+    console.error('❌ DB Error:', err);
+    res.status(500).json({ message: 'Failed to fetch client' });
+  }
+};
--- back-end/clients/create-client/src/routes/clients.routes.js
+++ back-end/clients/create-client/src/routes/clients.routes.js
@@ -4,10 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 // Verifica que la ruta sea correcta (usa dos puntos ../)
-const { createClient } = require('../controllers/clients.controller');
+const { createClient, getClientById } = require('../controllers/clients.controller');
 const validateClient = require('../middlewares/validateClient');
 
 
 router.post('/clients',validateClient, createClient);
+
+router.get('/clients/:id', getClientById);
+
 
 module.exports = router;
