9998231ddcc99baac99813a9cc74cd80fd5aedef
AlexMorales <admoralesp@uce.edu.ec>
2025-06-20 11:30:14 -0500

    feat: :sparkles: delete a service by id

---
+++ back-end/services-vet/delete-service/routes/service_routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/AlexTestz/delete-service/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api/services")
+	api.Delete("/:id", controllers.DeleteService)
+}
